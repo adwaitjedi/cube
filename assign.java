83de25f28d2cce485d43fe15acb2732252e64d63
Pranjalip-13 <anish.charak@somaiya.edu>
2023-02-13 10:17:15 +0530

    commited

---
+++ assign.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class assign{
+
+         public static void main(String[] args){
+                      int a = 2;
+	              int b = a*a*a;
+	              System.out.print(b);
+                                               }
+                   }
